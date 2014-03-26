d1cba7e7524ae8d18977c81cbfb794b71d671979
drus <drusunlimited@gmail.com>
2014-03-26 18:23:30 +0100

    AMD version
    For use with requirejs

--- imageStackLoader.js
+++ imageStackLoader.js
@@ -1,62 +1,66 @@
-var DRUS=DRUS || {};
-DRUS.ImageStackLoader=function(){
-	var my={
-		error_sg:new signals.Signal(),
-		progressed_sg:new signals.Signal(),
-		completed_sg:new signals.Signal()
-	};
+define(['jquery', 'lib/signals'], function($, signals){
 
-	var _list,
-		_total=0,
-		_index=0,
-		_loader=null,
-		_iebuggyStackOverflow;
+	return function(){
+		var my={
+			error_sg:new signals.Signal(),
+			progressed_sg:new signals.Signal(),
+			completed_sg:new signals.Signal()
+		};
 
-	function preload(imgs){
-		_list=imgs;
-		_total=imgs.length;
-		_index=0;
-		next();
-	}
+		var _list,
+			_total=0,
+			_index=0,
+			_loader=null,
+			_iebuggyStackOverflow;
 
-	function next(){
-		//log("index: ", _index, " - total: ", _total);
-		if (_index === _total){
-			my.completed_sg.dispatch();
-		}else{
-			_loader=$('<img />');
-			_loader.error(onError);
-			_loader.load(onLoad);
-			_loader.attr('src', _list[_index++]);
+		function preload(imgs){
+			_list=imgs;
+			_total=imgs.length;
+			_index=0;
+			next();
 		}
-	}
 
-	function onLoad(e) {
-		//log("onLoad index: ", _index, " - total: ", _total);
-		_loader.unbind();
-		_loader=null;
-		my.progressed_sg.dispatch(e, _index);
-		_iebuggyStackOverflow=setTimeout(next, 1);
-	}
+		function next(){
+			//log("index: ", _index, " - total: ", _total);
+			if (_index === _total){
+				my.completed_sg.dispatch();
+			}else{
+				_loader=$('<img />');
+				_loader.error(onError);
+				_loader.load(onLoad);
+				_loader.attr('src', _list[_index++]);
+			}
+		}
 
-	function onError(e) {
-		my.error_sg.dispatch(e, _index);
-		next();
-	}
-
-	function abort(){
-		if(_loader!==null){
-			_loader.load(null);
-			_loader.error(null);
+		function onLoad(e) {
+			//log("onLoad index: ", _index, " - total: ", _total);
+			_loader.unbind();
 			_loader=null;
+			my.progressed_sg.dispatch(e, _index);
+			_iebuggyStackOverflow=setTimeout(next, 1);
 		}
 
-		_index=0;
-		_total=0;
-		_list=null;
-	}
+		function onError(e) {
+			my.error_sg.dispatch(e, _index);
+			next();
+		}
 
-	my.preload=preload;
-	my.abort=abort;
-	return my;
-};
+		function abort(){
+			if(_loader!==null){
+				_loader.load(null);
+				_loader.error(null);
+				_loader=null;
+			}
+
+			_index=0;
+			_total=0;
+			_list=null;
+		}
+
+		my.preload=preload;
+		my.abort=abort;
+		return my;
+	};
+	
+
+});
